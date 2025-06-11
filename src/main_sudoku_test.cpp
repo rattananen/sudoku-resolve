b5bad13923ae6dbe53f420e923d3349403000d0f
jakk <rattananen@gmail.com>
2025-06-11 18:00:45 +0700

    update

--- src/main_sudoku_test.cpp
+++ src/main_sudoku_test.cpp
@@ -1,35 +1,53 @@
+#include <opencv2/opencv.hpp>
 #include <tesseract/baseapi.h>
-#include <leptonica/allheaders.h>
-
-#include <iostream>
 #include <memory>
+#include <iostream>
 
-int main()
-{
-    using Tess_str = std::unique_ptr<char[]>;
+int main(int argc, char* argv[]) {
 
-    tesseract::TessBaseAPI* api = new tesseract::TessBaseAPI();
-    // Initialize tesseract-ocr with English, without specifying tessdata path
-    if (api->Init(nullptr, "eng")) {
-        std::cerr << "Could not initialize tesseract.\n";
-        return 1;
-    }
+	using Tess_str = std::unique_ptr<char[]>;
+	std::string imPath{R"(E:\works\sudoku-resolve\assets\test_in.png)"};
 
-    // Open input image with leptonica library
-    Pix* image = pixRead("E:\\works\\sudoku-resolve\\assets\\test_in.png");
-    api->SetVariable("tessedit_char_whitelist", "0123456789");
-    api->SetImage(image);
-    // Get OCR result
-   Tess_str text(api->GetUTF8Text());
+	std::cout << "input img: " << imPath << "\n";
 
-   std::cout << "OCR output:\n" << text.get();
+	cv::Mat im = cv::imread(imPath);
 
 
-    // Destroy used object and release memory
-    api->End();
-    delete api;
-    //delete[] outText;
-    pixDestroy(&image);
 
-    return 0;
+	if (im.empty()) {
+		std::cerr << "cv can't decode img\n";
+		return 1;
+	}
+
+	//std::cout << "im:\n" << im << "\n";
+
+	cv::Mat tmp_im;
+
+	cv::cvtColor(im, tmp_im, cv::COLOR_BGR2GRAY);
+	//cv::blur(tmp_im, tmp_im, cv::Size(3, 3));
+
+	//std::cout << "tmp_im:\n" << tmp_im << "\n";
+
+	tesseract::TessBaseAPI api;
+
+	if (api.Init(nullptr, "eng")) {
+		std::cerr << "Could not initialize tesseract.\n";
+		return 1;
+	}
+	api.SetVariable("tessedit_char_whitelist", "0123456789");
+	api.SetPageSegMode(tesseract::PSM_SINGLE_BLOCK);
+	api.SetImage(im.data, im.cols, im.rows, 3, im.step);
+
+
+	Tess_str outText(api.GetUTF8Text());
+
+	std::cout << "output:\n" << outText.get();
+	api.End();
+
+
+	auto source_window = "test1";
+	cv::namedWindow(source_window);
+	cv::imshow(source_window, tmp_im);
+	cv::waitKey(0);
+	return 0;
 }
