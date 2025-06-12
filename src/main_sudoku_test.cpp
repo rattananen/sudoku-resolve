2ab4609c3a3570e9547c9924475bf328d9c8dd84
jakk <rattananen@gmail.com>
2025-06-12 11:47:07 +0700

    update

--- src/main_sudoku_test.cpp
+++ src/main_sudoku_test.cpp
@@ -22,6 +22,8 @@ void merge_lines(std::vector<cv::Vec4i>& lines, bool is_horizontal) {
 		cv::Vec4i& current_line = lines[i];
 		cv::Vec4i& last_merged_line = merged_lines.back();
 
+		
+
 		int current_pos = is_horizontal ? current_line[1] : current_line[0];
 		int last_merged_pos = is_horizontal ? last_merged_line[1] : last_merged_line[0];
 
@@ -39,19 +41,58 @@ void merge_lines(std::vector<cv::Vec4i>& lines, bool is_horizontal) {
 	lines = merged_lines;
 }
 
-void morph_line(cv::Mat& im, std::vector<cv::Vec4i>& lines, cv::Size size) {
-	cv::Mat horizontalStructure = cv::getStructuringElement(cv::MORPH_RECT, size);
-	cv::erode(im, im, horizontalStructure);
-	cv::dilate(im, im, horizontalStructure);
+//void merge_lines(const std::vector<cv::Vec4i>& lines, std::vector<cv::Vec4i>& merged,  bool is_horizontal) {
+//	if (lines.empty()) {
+//		return;
+//	}
+//
+//	std::sort(lines.begin(), lines.end(), [is_horizontal](const cv::Vec4i& a, const cv::Vec4i& b) {
+//		return (is_horizontal ? a[1] : a[0]) < (is_horizontal ? b[1] : b[0]);
+//		});
+//
+//	//std::vector<cv::Vec4i> merged_lines;
+//	merged.push_back(lines[0]);
+//
+//	for (size_t i = 1; i < lines.size(); ++i) {
+//		const cv::Vec4i& current_line = lines[i];
+//		cv::Vec4i& last_merged_line = merged.back();
+//
+//
+//
+//		int current_pos = is_horizontal ? current_line[1] : current_line[0];
+//		int last_merged_pos = is_horizontal ? last_merged_line[1] : last_merged_line[0];
+//
+//		// If the lines are close, merge them by extending the last merged line
+//		if (std::abs(current_pos - last_merged_pos) < 10) { // Merge threshold of 10 pixels
+//			last_merged_line[0] = std::min(last_merged_line[0], current_line[0]);
+//			last_merged_line[1] = std::min(last_merged_line[1], current_line[1]);
+//			last_merged_line[2] = std::max(last_merged_line[2], current_line[2]);
+//			last_merged_line[3] = std::max(last_merged_line[3], current_line[3]);
+//		}
+//		else {
+//			merged.push_back(current_line);
+//		}
+//	}
+//}
+
+void find_line(cv::Mat& im, std::vector<cv::Vec4i>& lines, cv::Size size) {
+	cv::Mat elem = cv::getStructuringElement(cv::MORPH_RECT, size);
+	cv::erode(im, im, elem);
+	cv::dilate(im, im, elem);
 
 	cv::HoughLinesP(im, lines, 1, CV_PI / 180, 80, 50, 10);
 }
 
+void draw_line(cv::Mat& im, std::vector<cv::Vec4i>& lines,const cv::Scalar& color) {
+	for ( auto& l : lines) {
+		cv::line(im, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), color, 2, cv::LINE_AA);
+	}
+}
 
 int main(int argc, char* argv[]) {
 
 	using Tess_str = std::unique_ptr<char[]>;
-	std::string imPath{R"(D:\works\sudoku-resolve\assets\test_in.png)"};
+	std::string imPath{R"(E:\works\sudoku-resolve\assets\test_in.png)"};
 
 	std::cout << "input img: " << imPath << "\n";
 
@@ -72,35 +113,22 @@ int main(int argc, char* argv[]) {
 	//cv::adaptiveThreshold(im_norm, im_norm, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY_INV, 11, 2);
 
 
+	cv::Mat im_with_table = im.clone();
+
+	cv::Mat im_line_process;
+	std::vector<cv::Vec4i> result_lines(50);
+
+	im_norm.copyTo(im_line_process);
+	find_line(im_line_process, result_lines, cv::Size(im_line_process.cols - 10, 1));
+	merge_lines(result_lines, true);
+	draw_line(im_with_table, result_lines, cv::Scalar(0, 255, 0));
+
+	im_norm.copyTo(im_line_process);
+	find_line(im_line_process, result_lines, cv::Size(1, im_line_process.rows - 10));
+	merge_lines(result_lines, false);
+	draw_line(im_with_table, result_lines, cv::Scalar(0, 0, 255));
 	 
-	cv::Mat horizontal = im_norm.clone();
-	std::vector<cv::Vec4i> horizontal_lines;
-	morph_line(horizontal, horizontal_lines, cv::Size(horizontal.cols - 10, 1));
-
 	
-	cv::Mat vertical = im_norm.clone();
-	std::vector<cv::Vec4i> vertical_lines;
-	morph_line(vertical, vertical_lines, cv::Size(1, vertical.rows - 10));
-
-
-	// Create an image to draw the detected lines
-	cv::Mat table_lines = im.clone();
-
-	// Merge close lines to form a cleaner grid
-	merge_lines(horizontal_lines, true);
-	merge_lines(vertical_lines, false);
-
-	// Draw horizontal lines
-	for (const auto& l : horizontal_lines) {
-	
-		cv::line(table_lines, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
-	}
-
-	// Draw vertical lines
-	for (const auto& l : vertical_lines) {
-		cv::line(table_lines, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
-	}
-
 
 	tesseract::TessBaseAPI api;
 
@@ -115,8 +143,7 @@ int main(int argc, char* argv[]) {
 
 	Tess_str outText(api.GetUTF8Text());
 
-	std::cout << "size h="  << horizontal_lines.size() << "\n";
-
+	
 	std::cout << "output:\n" << outText.get();
 	api.End();
 
@@ -126,7 +153,7 @@ int main(int argc, char* argv[]) {
 	auto im_tb_win = "table_lines";
 
 	cv::imshow(im_norm_win, im_norm);
-	cv::imshow(im_tb_win, table_lines);
+	cv::imshow(im_tb_win, im_with_table);
 
 
 	cv::waitKey(0);
